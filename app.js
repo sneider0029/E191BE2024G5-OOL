b33f94e46c22a282a5f0f99fcdc365a956fe908b
0sjumoca <osjunorm@gmail.com>
2024-04-28 10:43:46 -0500

    Separacion Routes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const rutaSede = require("./routes/SedeRouter");
-const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -11,26 +10,3 @@ app.use("/sede", rutaSede)
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT);
-
-//mongoose.connect("mongodb+srv://UTS:uts2024@uts.ccyqodk.mongodb.net/Prueba2024E191?retryWrites=true&w=majority&appName=UTS")
-//.then(() => console.log("Conectando a mongo"))
-//.catch(error => console.log(error));
-
-/*
-app.get("/", (req, res) => {
-    res.json({data:"hola con get"});
-});
-
-app.post("/", (req, res) => {
-    res.json({data: "Hola con post"});
-});
-
-app.delete("/:id", (req,res) => {
-    res.json({data: "hola con delete"})
-});
-
-app.patch("/:id", (req, res) =>{
-    res.json({data: "Hola con path"})
-});
-
-*/
