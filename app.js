de59df0c238afd2b695b7270ebbfc75d24f64e8a
0sjumoca <osjunorm@gmail.com>
2024-04-28 14:16:06 -0500

    Router Fix

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const rutaSede = require("./routes/SedeRouter");
+const rutaConsultorio = require("./routes/ConsultorioRouter");
+const rutaUbicacion = require("./routes/UbicacionRouter");
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/sede", rutaSede)
+app.use("/sede", rutaSede);
+app.use("/sede", rutaConsultorio);
+app.use("/sede", rutaUbicacion);
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT);
