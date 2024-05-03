e074c901dfc51fce2a9a1f07bbf55967051017dd
sneider0029 <sneiderortiz0029@gmail.com>
2024-05-03 11:09:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const rutaSede = require("./routes/SedeRouter");
-const rutaConsultorio = require("./routes/ConsultorioRouter");
-const rutaUbicacion = require("./routes/UbicacionRouter");
-
 app.use(cors());
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const rutaSede = require("./routes/SedeRouter");
+
 app.use("/sede", rutaSede);
-app.use("/sede", rutaConsultorio);
-app.use("/sede", rutaUbicacion);
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT);
+
+
